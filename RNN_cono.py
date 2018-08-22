b0d51912792a6f934a13f63a4b6178a558aa6b6f
GLASMAN Conor <c.glasman@bravosolution.com>
2018-08-22 17:30:13 +0100

    Finished the Numpy RNN, need to change to theano for GPU usage

--- RNN_cono.py
+++ RNN_cono.py
@@ -10,10 +10,42 @@ This will form part 1 of a larger project which will be to generate our own Flor
 import nltk
 import csv
 import itertools
+from datetime import datetime
 import numpy as np
 from CreateRNN import RNNNumpy
-nltk.download('punkt')
+import sys
+#nltk.download('punkt')
 
+
+# Outer SGD Loop
+    # - model: The RNN model instance
+    # - X_train: The training data set
+    # - y_train: The training data labels
+    # - learning_rate: Initial learning rate for SGD
+    # - nepoch: Number of times to iterate through the complete dataset
+    # - evaluate_loss_after: Evaluate the loss after this many epochs
+def train_with_sgd(model, X_train, y_train, learning_rate=0.005, nepoch=100, evaluate_loss_after=5):
+    # We keep track of the losses so we can plot them later
+    losses = []
+    num_examples_seen = 0
+    for epoch in range(nepoch):
+        # Optionally evaluate the loss
+        if (epoch % evaluate_loss_after == 0):
+            loss = model.calculate_loss(X_train, y_train)
+            losses.append((num_examples_seen, loss))
+            time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            print(f"{time}: Loss after num_examples_seen={num_examples_seen} epoch={epoch}: {loss}")
+            # Adjust the learning rate if loss increases
+            if (len(losses) < 1 and losses[-1][1] < losses[-2][1]):
+                learning_rate = learning_rate * 0.5 
+                print(f"Setting learning rate to {learning_rate}")
+            sys.stdout.flush()
+        # For each training example...
+        for i in range(len(y_train)):
+            # One SGD step
+            model.sgd_step(X_train[i], y_train[i], learning_rate)
+            num_examples_seen += 1
+             
 """
 We need to set certain variables here for use in the cleaning of the data set.
 The vocabulary_size limit imposed is in order to keep the training time down.
@@ -90,7 +122,20 @@ print("Expected Loss for random predictions: {}".format(np.log(vocabulary_size))
 print("Actual loss:{}".format(model.calculate_loss(X_train[:1000], y_train[:1000])))
 """
 
+
+"""
+This next chunk of code was used to have a quick test that the gradient check f
+function will work properly
 grad_check_vocab_size = 100
 np.random.seed(10)
 model = RNNNumpy(grad_check_vocab_size, 10, bptt_truncate=1000)
 model.gradient_check([0,1,2,3], [1,2,3,4])
+"""
+
+np.random.seed(10)
+model = RNNNumpy(vocabulary_size)
+%timeit model.numpy_sgd_step(X_train[10], y_train[10], 0.005)
+
+# Train on a small subset of the data to see what happens
+model = RNNNumpy(vocabulary_size)
+losses = train_with_sgd(model, X_train[:100], y_train[:100], nepoch=10, evaluate_loss_after=1)
